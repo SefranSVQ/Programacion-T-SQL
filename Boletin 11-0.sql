9e049624f28b8789e820c9fef2d47ffbb9e3ff1d
Sefran <sefran2992@gmail.com>
2018-04-23 14:17:36 +0200

    Actualizacion Enunciados
    Boletin 11-0 actualizado, enunciado boletin 11-1 añadido

--- Boletin 11-0.sql
+++ Boletin 11-0.sql
@@ -1,6 +1,20 @@
 -- Boletin 11.0
 -- Sobre la base de datos LeoTurf
 
+/* La Plantisha
+
+- Función [tipo]: 
+- Comentario: 
+- Cabecera:
+- Precondiciones: 
+- Entradas: 
+- Salidas:
+- E/S:
+- Postcondiciones:
+
+*/
+
+SET dateformat 'dMy'
 use leoturf
 go
 
@@ -8,14 +22,133 @@ go
 --	y nos devuelva el número de carreras disputadas por cada caballo entre esas dos fechas. 
 --	Las columnas serán ID (del caballo), nombre, sexo, fecha de nacimiento y número de carreras disputadas.
 
+/*
 
+- Función In-Line: FnCarrerasCaballo
+- Comentario: Esta función nos devolverá una tabla con los valores
+	ID del caballo, nombre, sexo, fecha de nacimiento y número de carreras disputadas
+	entre 2 fechas dadas.
+
+- Precondiciones: las fechas deben ser válidas
+- Entradas: FechaInicio (tipo fecha), FechaFin (tipo fecha)
+- Salidas: Tabla con ID del caballo, nombre, sexo, fecha de nacimiento y número de carreras disputadas
+- E/S: no hay.
+- Postcondiciones: Se habrá mostrado por pantalla una tabla con los valores
+	ID del caballo, nombre, sexo, fecha de nacimiento y número de carreras disputadas
+	entre llas 2 fechas dadas.
+
+*/
+
+
+alter function FnCarrerasCaballo ( @fechaInicio date, @fechaFin date ) 
+returns table as 
+return (
+
+	select CAB.ID, CAB.Nombre, CAB.Sexo, CAB.FechaNacimiento, COUNT(CAR.ID) as NumCarreras from LTCaballos as CAB
+	left join LTCaballosCarreras as CABCAR on CAB.ID = CABCAR.IDCaballo
+	left join LTCarreras as CAR on CABCAR.IDCarrera = CAR.ID
+	where CAR.Fecha between @fechaInicio and @fechaFin
+	group by CAB.ID, CAB.Nombre, CAB.Sexo, CAB.FechaNacimiento
+
+)
+
+go
+
+select * from FnCarrerasCaballo('11-01-2018','10-12-2018')
 
 -- 2.Crea una función escalar llamada FnTotalApostadoCC que reciba como parámetros el ID de un caballo 
 --	y el ID de una carrera y nos devuelva el dinero que se ha apostado a ese caballo en esa carrera.
 
+/*
+
+- Función Escalar: FnTotalApostadoCC
+- Comentario: Esta función calculará la cantidad monetaria apostada	
+	por un caballo en una carrera determinada.
+- Cabecera: FnTotalApostadoCC (@IDCaballo smallint, @IDCarrera smallint)
+- Precondiciones: las entradas deben ser valores positivos.
+- Entradas: IDCarrera (smaillint), IDCaballo (smallint)
+- Salidas: DineroApostado (money)
+- E/S: no hay.
+- Postcondiciones: se habrá devuelvo la cantidad monetaria total 
+	apostada por el caballo dado y en la carrera dada.
+
+*/
+
+go
+CREATE FUNCTION FnTotalApostadoCC (@IDCaballo smallint, @IDCarrera smallint) 
+RETURNS money AS 
+BEGIN 
+	
+	DECLARE @dinero money
+
+	set @dinero = (
+		select SUM(importe) from LTApuestas as APU
+		where IDCaballo = @IDCaballo and IDCarrera = @IDCarrera
+	)
+
+	RETURN @dinero
+
+END
+go
+
+print FnTotalApostadoCC(1,1)
+
 -- 3.Crea una función escalar llamada FnPremioConseguido que reciba como parámetros el ID de una apuesta 
 --	y nos devuelva el dinero que ha ganado dicha apuesta. Si todavía no se conocen las posiciones 
 --	de los caballos, devolverá un NULL
+
+/* 
+
+- Función Escalar: FnPremioConseguido
+- Comentario: esta función devolverá el premio de una apuesta dada. 
+	Si la carrera no ha finalizado, se devolverá null.
+- Cabecera: FnPremioConseguido (@IDApuesta int)
+- Precondiciones: el ID de la carrera debe ser un valor positivo.
+- Entradas: IDApuesta (int)
+- Salidas: Premio (money)
+- E/S: no hay
+- Postcondiciones: Se habrá devuelto el premio de una carrera dada. 
+	Si la carrera no ha finalizado, se devolverá null.
+
+*/
+
+go
+
+create function FnPremioConseguido (@IDApuesta int)
+returns int as 
+begin
+	declare @premio money
+
+	if (
+		select CABCAR.Posicion from LTApuestas as APU
+		inner join LTCaballosCarreras as CABCAR on APU.IDCaballo = CABCAR.IDCaballo and APU.IDCarrera = CABCAR.IDCarrera
+		where APU.ID = 1
+	
+	) = 1
+	begin
+		set money = ( -- mal
+			select CABCAR.Premio1 from LTApuestas as APU
+			inner join LTCaballosCarreras as CABCAR on APU.IDCaballo = CABCAR.IDCaballo and APU.IDCarrera = CABCAR.IDCarrera
+			where APU.ID = 1
+		)
+	end
+	else if (
+			select CABCAR.Posicion from LTApuestas as APU
+			inner join LTCaballosCarreras as CABCAR on APU.IDCaballo = CABCAR.IDCaballo and APU.IDCarrera = CABCAR.IDCarrera
+			where APU.ID = 1
+	
+		) = 2
+		begin
+
+		end
+		else set money = null
+
+
+return @premio
+end
+
+go
+
 
 -- 4.El procedimiento para calcular los premios en las apuestas de una carrera 
 --	(los valores que deben figurar en la columna Premio1 y Premio2) es el siguiente:
--- Boletin 11-1.sql
+++ Boletin 11-1.sql
@@ -1,26 +1,63 @@
--- 1. Deseamos incluir un producto en la tabla Products llamado "Cruzcampo lata” pero no estamos seguros si se ha insertado o no.
--- El precio son 4,40, el proveedor es el 16, la categoría 1 y la cantidad por unidad es "Pack 6 latas” 
--- "Discontinued” toma el valor 0 y el resto de columnas se dejarán a NULL.
--- Escribe un script que compruebe si existe un producto con ese nombre. 
--- En caso afirmativo, actualizará el precio y en caso negativo insertarlo. 
+/*
 
--- 2. Comprueba si existe una tabla llamada ProductSales. Esta tabla ha de tener de cada producto el ID, 
--- el Nombre, el Precio unitario, el número total de unidades vendidas y el total de dinero 
--- facturado con ese producto. Si no existe, créala
+1. Crea una función a la que pasemos un intervalo de tiempo y nos devuelva una tabla con el Happy day 
+(el día que más ha ganado) y el black day (el día que más ha perdido) de cada jugador. 
+Si hay más de un día en que haya ganado o perdido el máximo, tomaremos el más reciente. 
+Columnas: ID del jugador, nombre, apellidos, fecha del happy day, cantidad ganada, fecha del black day, cantidad perdida.
 
--- 3. Comprueba si existe una tabla llamada ShipShip. Esta tabla ha de tener de cada Transportista el ID, 
--- el Nombre de la compañía, el número total de envíos que ha efectuado y el número de países 
--- diferentes a los que ha llevado cosas. Si no existe, créala.
+*/
 
--- 4. Comprueba si existe una tabla llamada EmployeeSales. Esta tabla ha de tener de cada empleado su ID, 
---  el Nombre completo, el número de ventas totales que ha realizado, el número de clientes diferentes 
---  a los que ha vendido y el total de dinero facturado. Si no existe, créala.
+/*
+2.Se ha creado un coeficiente para valorar los caballos. Su valor se calcula sumando el 
+número de carreras ganadas multiplicado por cinco más el número de carreras en las que 
+ha quedado segundo multiplicado por tres. El resultado se divide entre el número de 
+carreras disputadas multiplicado por 0,2. Al resultado de todo eso de lo multiplica 
+por un coeficiente de edad que se calcula según la tabla siguiente:
 
--- 5. Entre los años 96 y 97 hay productos que han aumentado sus ventas y otros que las han disminuido. 
---	Queremos cambiar el precio unitario según la siguiente tabla:
 
--- Incremento de ventas		Incremento de precio
---		Negativo					-10%
---	  Entre 0 y 10%				  No varía
---	  Entre 10% y 50%				+5%
---    Mayor del 50%      10% con un máximo de 2,25
+Edad					Valor
+
+Seis o menos años		100
+
+Siete					90
+
+Ocho o nueve			75
+
+Diez					65
+
+Más de diez				40
+
+*/
+
+/*
+
+3.Queremos saber la cantidad de dinero en apuestas que mueve cada hipódromo. Haz una función 
+a la que se le pase un rango de fechas y nos devuelva el dinero movido en apuestas en cada 
+hipódromo entre esas fechas. También queremos saber cuál fue la apuesta más alta de ese periodo. 
+Considerar solo las apuestas, no los premios. Columnas: Nombre del hipódromo, cantidad gestionada, 
+fecha de la apuesta más alta, importe de la apuesta más alta y otra columna que tomará 
+los valores G,C o P según si esa apuesta acertó el primero (Ganador), 
+el segundo (Colocado) o no obtuvo premio (Pierde).
+
+*/
+
+/*
+
+4.Haz una función DescalificaCaballo que reciba como parámetros el ID de un Caballo y 
+en ID de una carrera y descalifique a ese caballo en esa carrera. Eso puede dar lugar, 
+si el caballo quedó primero o segundo, a que haya que alterar los premios obtenidos.
+
+	a.Si el caballo descalificado fue primero: Crear apuntes para descontar los premios 
+	obtenidos por los que apostaron por él, anular también los apuntes de los que 
+	apostaron por el segundo, que ahora pasa a ser primero y generar los apuntes 
+	correspondientes al nuevo premio. Crear los apuntes correspondientes al segundo 
+	premio para los que apostaron por el tercero, que ahora pasa a ser segundo.
+
+	b.Si el caballo descalificado fue segundo: Anular las ganancias de los que apostaron 
+	por él. Crear los apuntes correspondientes al segundo premio para los que 
+	apostaron por el tercero, que ahora pasa a ser segundo.
+
+La función nos devolverá los apuntes que haya que insertar en la tabla Apuntes. 
+No se borra ningún apunte. Los que ya no sirvan se crea uno con el importe opuesto.
+
+*/
